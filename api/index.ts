0341ba26e7055d0d31c44c445b840cbddd6ddda7
matsudashinsuke <smatsuda@x-hack.jp>
2024-07-30 12:06:28 +0900

    画像解説APIの雛形を作成

--- api/index.ts
+++ api/index.ts
@@ -1,13 +1,58 @@
+const dotenv = require("dotenv");
 const express = require("express");
+const OpenAI = require("openai");
+
+// expressアプリケーションを作成
 const app = express();
+// JSONボディパーサーを使用
+app.use(express.json());
+// 環境変数を読み込む
+dotenv.config();
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
-
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
-// simple post method
-app.post("/post", (req, res) => {
-    res.send("Post method called");
+//  curl -X POST -H "Content-Type: application/json" -d '{"image_url":"https://sample/image.jpg"}' http://localhost:3000/post
+app.post("/post", async (req: any, res: any) => {
+	console.log({ body: req.body });
+	const content = await main(req.body.image_url);
+
+	res.send(content);
 });
+
+async function main(image_url: string) {
+	try {
+		// 初期化
+		const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+
+		const response = await openai.chat.completions.create({
+			model: "gpt-4o-2024-05-13",
+			max_tokens: 4096,
+			// prompt: "あなたは画像を見ています。画像について説明してください。",
+			messages: [
+				{
+					role: "user",
+					content: [
+						{
+							type: "text",
+							text: "この画像について説明してください。"
+						},
+						{
+							type: "image_url",
+							image_url: {
+								"url": image_url,
+							},
+						},
+					],
+				},
+			],
+		});
+		console.log({ content: response.choices[0]["message"]["content"] });
+		return response.choices[0]["message"]["content"];
+	} catch (error) {
+		console.error(error);
+		return error;
+	}
+}
 
 module.exports = app;
