bb1b2e8fe264441990d675a2662e23e11b74276a
matsudashinsuke <smatsuda@x-hack.jp>
2024-11-07 12:30:21 +0900

    update

--- README.md
+++ README.md
@@ -7,23 +7,31 @@
 
 # 準備
 1. OpenAI API Key を取得する
-  .env に keyを登録する
+  API KEY を取得しておく
 
 2. git clone 
   ```
   $ git clone https://github.com/X-HACK/open-ai-sample.git
   ```
+
 3. change directory
   ```
   $ cd open-ai-sample
   ```
+
 4. npm install
   ```
   $ npm install
   ```
+
 5. vercel login
   ```
   $ vercel login
   ```
 
+6. `.env` ファイルを作成して、keyを登録する
+  ```
+  $ cp .env.sample .env
+  ```
+  .envファイルに、step.1 で取得したkeyをセットする
   
--- api/index.ts
+++ api/index.ts
@@ -2,6 +2,9 @@ const dotenv = require("dotenv");
 const express = require("express");
 const OpenAI = require("openai");
 
+// トークンの最大長を設定
+const MAX_TOKEN_SIZE = 4096;
+
 // expressアプリケーションを作成
 const app = express();
 // JSONボディパーサーを使用
@@ -9,92 +12,91 @@ app.use(express.json());
 // 環境変数を読み込む
 dotenv.config();
 
+// ルートエンドポイント
 app.get("/", (req, res) => res.send("Express on Vercel"));
 app.get("/hello", (req, res) => res.send("Hello, World!"));
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log("Server ready on port 3000."));
 
+// 使用例
 // curl -X POST -H "Content-Type: application/json" -d '{"image_url":"https://sample/image.jpg"}' http://localhost:3000/post
 app.post("/post", async (req, res) => {
-	console.log({ body: req.body });
+  console.log({ body: req.body });
 
-	// parameter に text か image_url がない場合は 200 で返す
-	if (!req.body.text && !req.body.image_url) {
-		res.status(200).send("text または image_url を指定してください。");
-		return;
-	}
+  // parameter に text か image_url がない場合は 200 で返す
+  if (!req.body.text && !req.body.image_url) {
+    res.status(200).send("text または image_url を指定してください。");
+    return;
+  }
 
-	let content = "";
-	if (req.body.text) {
-		content = await requestTextDescription(req.body.text);
-	} else if (!req.body.image_url.match(/^https?:\/\/.*\.(png|jpg|jpeg|gif)$/)) {
-		content = await requestImageDescription(req.body.image_url);
-	}
-
-	res.send(content);
+  if (req.body.text) {
+    res.send(await requestTextDescription(req.body.text));
+  } else if (!req.body.image_url.match(/^https?:\/\/.*\.(png|jpg|jpeg|gif)$/)) {
+    res.send(await requestImageDescription(req.body.image_url));
+  }
 });
 
 // 画像について質問する
-async function requestImageDescription(image_url) {
-	try {
-		// 初期化
-		const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
-		const response = await openai.chat.completions.create({
-			model: "gpt-4o-2024-05-13",
-			max_tokens: 4096,
-			// prompt: "あなたは画像を見ています。画像について説明してください。",
-			messages: [
-				{
-					role: "user",
-					content: [
-						{
-							type: "text",
-							text: "この画像について説明してください。"
-						},
-						{
-							type: "image_url",
-							image_url: {
-								"url": image_url,
-							},
-						},
-					],
-				},
-			],
-		});
-		console.log({ content: response.choices[0]["message"]["content"] });
-		return response.choices[0]["message"]["content"];
-	} catch (error) {
-		console.error(error);
-		return error;
-	}
+async function requestImageDescription(image_url: any) {
+  try {
+    // 初期化
+    const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+    const response = await openai.chat.completions.create({
+      model: "gpt-4o-2024-05-13",
+      max_tokens: MAX_TOKEN_SIZE,
+      messages: [
+        {
+          role: "user",
+          content: [
+            {
+              type: "text",
+              text: "この画像について説明してください。"
+            },
+            {
+              type: "image_url",
+              image_url: {
+                "url": image_url,
+              },
+            },
+          ],
+        },
+      ],
+    });
+    console.log({ content: response.choices[0]["message"]["content"] });
+    return response.choices[0]["message"]["content"];
+  } catch (error) {
+    console.error(error);
+    return error;
+  }
 }
 
-// 画像について質問する
-async function requestTextDescription(text) {
-	try {
-		// 初期化
-		const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
-		const response = await openai.chat.completions.create({
-			model: "gpt-4o-2024-05-13",
-			max_tokens: 4096,
-			messages: [
-				{
-					role: "user",
-					content: [
-						{
-							type: "text",
-							text: text
-						},
-					],
-				},
-			],
-		});
-		console.log({ content: response.choices[0]["message"]["content"] });
-		return response.choices[0]["message"]["content"];
-	} catch (error) {
-		console.error(error);
-		return error;
-	}
+// テキストで質問する
+async function requestTextDescription(text: any) {
+  try {
+    // 初期化
+    const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+    const response = await openai.chat.completions.create({
+      model: "gpt-4o-2024-05-13",
+      max_tokens: MAX_TOKEN_SIZE,
+      messages: [
+        {
+          role: "user",
+          content: [
+            {
+              type: "text",
+              text: text
+            },
+          ],
+        },
+      ],
+    });
+    console.log({ content: response.choices[0]["message"]["content"] });
+    return response.choices[0]["message"]["content"];
+  } catch (error) {
+    console.error(error);
+    return error;
+  }
 }
 
 module.exports = app;
